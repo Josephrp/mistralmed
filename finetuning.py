8593611cd3a1d8b8d284e229db8ec4e08e5aa691
Tonic <josephrichardpollack@gmail.com>
2023-10-20 15:55:57 +0200

    Create finetuning.py

---
+++ finetuning.py
@@ -0,0 +1,211 @@
+# MistralMED outshines other language models when it comes to answering complex medical questions, providing accurate and powerful insights for researchers.
+
+from accelerate import FullyShardedDataParallelPlugin, Accelerator
+from torch.distributed.fsdp.fully_sharded_data_parallel import FullOptimStateDictConfig, FullStateDictConfig
+
+fsdp_plugin = FullyShardedDataParallelPlugin(
+    state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=False),
+    optim_state_dict_config=FullOptimStateDictConfig(offload_to_cpu=True, rank0_only=False),
+)
+
+accelerator = Accelerator(fsdp_plugin=fsdp_plugin)
+
+from datasets import load_dataset
+
+train_dataset = load_dataset('keivalya/MedQuad-MedicalQnADataset', split='train')
+
+print(train_dataset)
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+base_model_id = "mistralai/Mistral-7B-v0.1"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(base_model_id, quantization_config=bnb_config)
+
+tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    model_max_length=512,
+    padding_side="left",
+    add_eos_token=True)
+
+tokenizer.pad_token = tokenizer.eos_token
+
+def tokenize(prompt):
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=512,
+        padding="max_length",
+    )
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+def generate_and_tokenize_prompt(data_point):
+    full_prompt =f"""Given a target sentence construct the underlying meaning representation of the input sentence as a single function with attributes and attribute values.
+This function should describe the target string accurately and the function must be one of the following ['inform', 'request', 'give_opinion', 'confirm', 'verify_attribute', 'suggest', 'request_explanation', 'recommend', 'request_attribute'].
+The attributes must be one of the following: ['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating', 'genres', 'player_perspective', 'has_multiplayer', 'platforms', 'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier']
+
+### Target sentence:
+{data_point["Question"]}
+
+### Meaning representation:
+{data_point["Answer"]}
+"""
+    return tokenize(full_prompt)
+
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
+
+print(tokenized_train_dataset[4]['input_ids'])
+
+print(len(tokenized_train_dataset[4]['input_ids']))
+
+eval_prompt = """Given a target sentence construct the underlying meaning representation of the input sentence as a single function with attributes and attribute values.
+This function should describe the target string accurately and the function must be one of the following ['inform', 'request', 'give_opinion', 'confirm', 'verify_attribute', 'suggest', 'request_explanation', 'recommend', 'request_attribute'].
+The attributes must be one of the following: ['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating', 'genres', 'player_perspective', 'has_multiplayer', 'platforms', 'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier']
+
+### Target sentence:
+Earlier, you stated that you didn't have strong feelings about PlayStation's Little Big Adventure. Is your opinion true for all games which don't have multiplayer?
+
+### Meaning representation:
+"""
+
+model_input = tokenizer(eval_prompt, return_tensors="pt").to("cuda")
+
+model.eval()
+with torch.no_grad():
+    print(tokenizer.decode(model.generate(**model_input, max_new_tokens=256, pad_token_id=2)[0], skip_special_tokens=True))
+
+from peft import prepare_model_for_kbit_training
+
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+from peft import LoraConfig, get_peft_model
+
+config = LoraConfig(
+    r=8,
+    lora_alpha=16,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+        "lm_head",
+    ],
+    bias="none",
+    lora_dropout=0.05,  # Conventional
+    task_type="CAUSAL_LM",
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+# Apply the accelerator. You can comment this out to remove the accelerator.
+model = accelerator.prepare_model(model)
+
+print(model)
+
+if torch.cuda.device_count() > 1: # If more than 1 GPU
+    model.is_parallelizable = True
+    model.model_parallel = True
+
+import transformers
+from datetime import datetime
+
+project = "mistralmed"
+base_model_name = "mistral"
+run_name = base_model_name + "-" + project
+output_dir = "./" + run_name
+
+tokenizer.pad_token = tokenizer.eos_token
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=tokenized_train_dataset,
+  #  eval_dataset=tokenized_val_dataset,
+    args=transformers.TrainingArguments(
+        output_dir=output_dir,
+        warmup_steps=5,
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=4,
+        max_steps=1000,
+        learning_rate=2.5e-5, # Want about 10x smaller than the Mistral learning rate
+        logging_steps=50,
+        bf16=True,
+        optim="paged_adamw_8bit",
+        logging_dir="./logs",        # Directory for storing logs
+        save_strategy="steps",       # Save the model checkpoint every logging step
+        save_steps=50,                # Save checkpoints every 50 steps
+   #     evaluation_strategy="steps", # Evaluate the model every logging step
+   #     eval_steps=50,               # Evaluate and save checkpoints every 50 steps
+   #     do_eval=False,                # Perform evaluation at the end of training
+   #     report_to="wandb",           # Comment this out if you don't want to use weights & baises
+   #     run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}"          # Name of the W&B run (optional)
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+trainer.train()
+
+api_token = "YOURAPITOKENHERE"
+base_model = AutoModelForCausalLM.from_pretrained(
+    base_model_id,  # Mistral, same as before
+    quantization_config=bnb_config,  # Same quantization config as before
+    device_map="auto",
+    trust_remote_code=True,
+    use_auth_token=api_token
+)
+
+tokenizer = AutoTokenizer.from_pretrained(base_model_id, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+
+from peft import PeftModel
+
+finetuned_model = PeftModel.from_pretrained(base_model, "mistral-mistralmed/checkpoint-1000")
+
+
+# If you haven't logged in or stored your API token in the cache, you can provide it as follows:
+api_token = "YOURTOKENHERE"
+
+# Push your finetuned model to the hub
+from transformers import AutoModelForSequenceClassification
+
+# Assuming you have a finetuned model
+#finetuned_model = AutoModelForSequenceClassification.from_pretrained("mistralmed/checkpoint-1000")
+
+# Push your model to the hub
+repo_name = "mistralmed"  # The name of the repository in your namespace
+private = False  # Set to True if you want the model to be private
+
+# If you have your API token in the cache, you can push your model like this
+#finetuned_model.push_to_hub(repo_name, use_auth_token=True, organization=None, private=private)
+
+# If you don't have your API token in the cache, provide it explicitly
+finetuned_model.push_to_hub(repo_name, use_auth_token=api_token, organization=None, private=private)
+
+# Your model is now available on the Hugging Face model hub under your namespace.
